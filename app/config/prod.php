6f13a111a8887e14c8a1c850ca8adb76d0e25c4c
DELAGE <DELAGE@INFOGESTION.FR>
2017-04-06 14:26:13 +0200

    Base du projet PHP

---
+++ app/config/prod.php
@@ -0,0 +1,12 @@
+<?php
+
+// Doctrine (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'charset'  => 'utf8',
+    'host'     => '172.17.21.15',
+    'port'     => '3306',
+    'dbname'   => 'gsbphp',
+    'user'     => 'root',
+    'password' => 'mdp',
+);
---
+++ app/routes.php
@@ -0,0 +1,36 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use PPE_PHP_GSB\Domain\User;
+use PPE_PHP_GSB\Form\Type\UserType;
+
+
+//route appelée de base : authentification
+$app->get('/', function(Request $request) use ($app) {
+    return $app['twig']->render('login.html.twig', array(
+        'error'         => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+    ));
+})->bind('home');
+
+// Login 
+$app->get('/login', function(Request $request) use ($app) {
+    return $app['twig']->render('login.html.twig', array(
+        'error'         => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
+    ));
+})->bind('login');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
